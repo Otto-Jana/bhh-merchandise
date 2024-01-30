1e1a55c29531f3cb846432c0b5b34e548bb0b8e4
Otto-Joshua <Joshua.Bilek@otto.de>
2024-01-30 15:38:01 +0100

    ire

--- php_scripts/handle_sql_requests.php
+++ php_scripts/handle_sql_requests.php
@@ -53,9 +53,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Top Sellers - BHHxITECH</title>
-    <link rel="stylesheet" href="css/shop_tiles.css">
+    <link rel="stylesheet" href="../css/shop_tiles.css">
     <link rel="stylesheet" href="https://use.typekit.net/bsc2ugu.css">
-    <script src="./static/js/shop_tiles.js" defer></script>
+    <script src="../static/js/shop_tiles.js" defer></script>
 </head>
 <body>
 
---
+++ static/js/shop_tiles.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+
+
+var productName = "<?= $row['pro_name'] ?>";
+console.log(productName);
+};
