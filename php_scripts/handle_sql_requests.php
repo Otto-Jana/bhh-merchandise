0ccb1d05f8664312046fc68451daea6b6ff32980
Otto-Joshua <Joshua.Bilek@otto.de>
2024-01-30 14:07:55 +0100

    gfsdggs

--- php_scripts/handle_sql_requests.php
+++ php_scripts/handle_sql_requests.php
@@ -5,6 +5,8 @@
 	$password = "040BeruflicheHochschule";
 	$dbname = "merchandise_shop";
 	$InputValue = $_COOKIE["tileType"];
+	$InputValue = (int)$InputValue;
+	
 
 	 // Create connection
 	$conn = mysqli_connect(hostname: $host, username: $username, password: $password, database: $dbname);
@@ -12,31 +14,33 @@
 	{
 	die("Connection Error:" . mysqli_connect_error());
 	}
-	echo "<script> import { returnedRows } from './main.js;' console.log(returnedRows); </script>";
-
-
-
-
-
+	
 	//               GET NECESSARY TILE INFORMATION FROM DATABASE
+	int();
 
+	if(InputValue = 1) //Best Sellers
+	{
+		$sql = "SELECT * FROM product ORDER BY sellingScore DESC LIMIT 10";
 
-	// if(InputValue = 1) //Best Sellers
-	// {
-	// 	$sql = "SELECT * FROM product ORDER BY sellingScore DESC LIMIT 10";
-
-	// }
-	// else if(InputValue = 2) //For You
-	// {
+	}
+	else if(InputValue = 2) //For You
+	{
 		
-	// }
-	// else { //New Arrivals
-	// 	$sql = "SELECT * FROM Products ORDER BY Date_Added DESC LIMIT 50";
-	// }
+	}
+	else { //New Arrivals
+		$sql = "SELECT * FROM Products ORDER BY Date_Added DESC LIMIT 50";
+	}
 
-	// $result = $conn->query($sql);
-	// $row =	$result->fetch_assoc();
-	// $returnedRows =  json_encode_($row);
+	$result = $conn->query($sql);
+	$row =	$result->fetch_assoc();
+	$returnedRows =  json_encode_($row);
+
+	$file = fopen('../temp/tile-list.json','w');
+	fwrite($file, $returnedRows);
+	fclose($file);
+
+	echo "<script>  window.location.href ='../shop_tiles.html';</script>"
+
 
 ?>
 
--- static/js/shop_tiles.js
+++ static/js/shop_tiles.js
@@ -1,1 +1,16 @@
-window.location.href = "handle_sql_requests.php?tileType=" + 2;
+function fetchJSONData() {
+    fetch("./temp/tile-list.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => 
+              console.log(data))
+        .catch((error) => 
+               console.error("Unable to fetch data:", error));
+}
+
+fetchJSONData();
