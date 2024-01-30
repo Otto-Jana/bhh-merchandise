0df493fb67c590c21684616422888b1babbe7ee8
Otto-Joshua <Joshua.Bilek@otto.de>
2024-01-30 14:09:02 +0100

    fhe

--- php_scripts/handle_sql_requests.php
+++ php_scripts/handle_sql_requests.php
@@ -35,11 +35,11 @@
 	$row =	$result->fetch_assoc();
 	$returnedRows =  json_encode_($row);
 
-	$file = fopen('../temp/tile-list.json','w');
-	fwrite($file, $returnedRows);
-	fclose($file);
+	// $file = fopen('../temp/tile-list.json','w');
+	// fwrite($file, $returnedRows);
+	// fclose($file);
 
-	echo "<script>  window.location.href ='../shop_tiles.html';</script>"
+	// echo "<script>  window.location.href ='../shop_tiles.html';</script>"
 
 
 ?>
