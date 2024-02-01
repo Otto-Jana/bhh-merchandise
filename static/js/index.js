fc2138789abaf8909e64b82e0270eef503e2cd09
Otto-Joshua <Joshua.Bilek@otto.de>
2024-02-01 02:05:48 +0100

    re:

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,11 @@
 leftButton = document.getElementById("left-button");
 rightButton = document.getElementById("right-button");
 slideshowcontent = document.getElementById("content");
+// Get all elements from index.html
+var button1 = document.getElementById("top_seller_bttn").onclick = HandleTopSellerTrigger;
+document.getElementById("fuer_dich_bttn").onclick = HandleForYouTrigger;
+document.getElementById("new_arrivals_bttn").onclick = HandleNewArrivalTrigger;
+console.log("Setup all triggers");
 
 let prevSlide = 0;
 let slideBlock = false;
@@ -49,3 +54,28 @@ Array.from(document.getElementsByClassName("toggle-menu")).forEach(element => {
 })
 
 
+
+
+function HandleTopSellerTrigger() {
+    document.cookie="tileType=1";
+    console.log(document.cookie);
+    console.log("Succsessfuly set the cookie: tileType=1");
+    window.location.href ="./php_scripts/handle_sql_requests.php";
+  
+};
+
+function HandleForYouTrigger() {
+    document.cookie="tileType=3";
+    console.log("Succsessfuly set the cookie: tileType=2");
+    window.location.href ="./php_scripts/handle_sql_requests.php";
+
+};
+
+function HandleNewArrivalTrigger() {
+
+    document.cookie="tileType=3";
+    console.log("Succsessfuly set the cookie: tileType=3");
+    window.location.href ="./php_scripts/handle_sql_requests.php";
+};
+
+ 
