c645131799bcb190002aca7f78596d2a02bd2904
Otto-Joshua <Joshua.Bilek@otto.de>
2024-02-01 02:03:53 +0100

    rer

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,12 @@
 
 
-window.onload = function(){
+
 // Get all elements from index.html
 var button1 = document.getElementById("top_seller_bttn").onclick = HandleTopSellerTrigger;
 // document.getElementById("fuer_dich_bttn").onclick = HandleForYouTrigger;
 // document.getElementById("new_arrivals_bttn").onclick = HandleNewArrivalTrigger;
 console.log("Setup all triggers");
-};
+
 
 function HandleTopSellerTrigger() {
     document.cookie="tileType=1";
